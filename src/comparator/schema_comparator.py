b45796a1c20f88bea855fcd7083432d490fb57db
BigPotato <pyn777777@gmail.com>
2019-11-17 18:38:49 -0500

    comparator v0.1

---
+++ src/comparator/schema_comparator.py
@@ -0,0 +1,53 @@
+import json
+from src.converter.const import SchemaKey, TypeKey, LoggingMessage, TypeToTypeKeyMap
+
+
+class SchemaComparator:
+    def __init__(self):
+        pass
+
+    def compare(self, dic1: dict, dic2: dict):
+        for key in dic1:
+            if key in dic2:
+                # 如果两个schema都不是dict类型
+                if type(dic1[key]) != dict and type(dic2[key]) != dict:
+                    if dic1[key] != dic2[key]:
+                        # 打印Schema1
+                        print("Schema1(path: ", end="")
+                        print(dic1['path'] + "): ", end="")
+                        print('"' + key + '"' + ": " + str(dic1[key]), end="")
+                        print(";    ", end="")
+                        # 打印Schema2
+                        print("Schema2(path: ", end="")
+                        print(dic2['path'] + "): ", end="")
+                        print('"' + key + '"' + ": " + str(dic2[key]))
+                elif type(dic1[key]) == dict and type(dic2[key]) == dict:
+                    self.compare(dic1[key], dic2[key])
+            else:
+                print("Schema1(path: ", end="")
+                print(dic1['path'] + "): ", end="")
+                print('"' + key + '"' + ": " + str(dic1[key]), end="")
+                print(";    ", end="")
+                print("Schema2: no such key found")
+
+        for key in dic2:
+            if key not in dic1:
+                print("Schema2(path: ", end="")
+                print(dic2['path'] + "): ", end="")
+                print('"' + key + '"' + ": " + str(dic2[key]), end="")
+                print(";    ", end="")
+                print("Schema1: no such key found")
+
+    def compare_schema(self, schema1, schema2):
+        with open(schema1, 'r') as f:
+            schema1_dict = json.load(f)
+        with open(schema2, 'r') as f:
+            schema2_dict = json.load(f)
+        self.compare(schema1_dict, schema2_dict)
+
+
+if __name__ == "__main__":
+    comparator = SchemaComparator()
+    schema1 = '../../schema_example/schema_case_002'
+    schema2 = '../../schema_example/schema_case_003'
+    comparator.compare_schema(schema1, schema2)
