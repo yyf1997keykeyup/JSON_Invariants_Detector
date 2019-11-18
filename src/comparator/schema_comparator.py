aa221203f971892cfea8adda9d99fac3f043dd67
BigPotato <pyn777777@gmail.com>
2019-11-18 16:43:25 -0500

    comparator v0.2 解决了path定位问题

--- src/comparator/schema_comparator.py
+++ src/comparator/schema_comparator.py
@@ -6,7 +6,7 @@ class SchemaComparator:
     def __init__(self):
         pass
 
-    def compare(self, dic1: dict, dic2: dict):
+    def compare(self, dic1: dict, dic2: dict, path1="#", path2="#"):
         for key in dic1:
             if key in dic2:
                 # 如果两个schema都不是dict类型
@@ -14,18 +14,18 @@ class SchemaComparator:
                     if dic1[key] != dic2[key]:
                         # 打印Schema1
                         print("Schema1(path: ", end="")
-                        print(dic1['path'] + "): ", end="")
+                        print(path1 + "): ", end="")
                         print('"' + key + '"' + ": " + str(dic1[key]), end="")
                         print(";    ", end="")
                         # 打印Schema2
                         print("Schema2(path: ", end="")
-                        print(dic2['path'] + "): ", end="")
+                        print(path2 + "): ", end="")
                         print('"' + key + '"' + ": " + str(dic2[key]))
                 elif type(dic1[key]) == dict and type(dic2[key]) == dict:
-                    self.compare(dic1[key], dic2[key])
+                    self.compare(dic1[key], dic2[key], str(path1+'/'+key), str(path2+'/'+key))
             else:
                 print("Schema1(path: ", end="")
-                print(dic1['path'] + "): ", end="")
+                print(path1 + "): ", end="")
                 print('"' + key + '"' + ": " + str(dic1[key]), end="")
                 print(";    ", end="")
                 print("Schema2: no such key found")
@@ -33,7 +33,7 @@ class SchemaComparator:
         for key in dic2:
             if key not in dic1:
                 print("Schema2(path: ", end="")
-                print(dic2['path'] + "): ", end="")
+                print(path2 + "): ", end="")
                 print('"' + key + '"' + ": " + str(dic2[key]), end="")
                 print(";    ", end="")
                 print("Schema1: no such key found")
